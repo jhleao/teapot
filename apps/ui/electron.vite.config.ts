dd0f1e3fb7e4b794336c32c9daf4278910eddfa4
jhleao <77419998+jhleao@users.noreply.github.com>
2025-11-15 17:41:45 +0000

    chore: init electron

---
+++ apps/ui/electron.vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    // @ts-expect-error - benign type version mismatch
+    plugins: [react(), tailwindcss()]
+  }
+})

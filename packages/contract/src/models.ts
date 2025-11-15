9ed661e5610bafae9c7bba7d3ca0769db1491017
jhleao <77419998+jhleao@users.noreply.github.com>
2025-11-15 17:22:40 +0000

    feat: improve models

--- packages/contract/src/models.ts
+++ packages/contract/src/models.ts
@@ -1,16 +1,24 @@
-type Contract = {};
-
 export type Repo = {
   path: string;
+  commits: Commit[];
   branches: Branch[];
 };
 
 export type Branch = {
   ref: string;
   isTrunk: boolean;
+  isRemote: boolean;
+  headSha: string;
+};
+
+export type Commit = {
+  sha: string;
+  message: string;
+  timeMs: number;
+  parentSha: string;
+  childrenSha: string[];
 };
 
 export type Configuration = {
   repoPath: string;
 };
-

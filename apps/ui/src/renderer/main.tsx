e94527fb222c7b8b956489609d3f60794664a77d
José Henrique Leão <jhleao99@gmail.com>
2025-11-16 09:38:15 -0300

    feat: drag and drop (#19)

--- apps/ui/src/renderer/main.tsx
+++ apps/ui/src/renderer/main.tsx
@@ -3,9 +3,12 @@ import './assets/main.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { GlobalProvider } from './contexts/GlobalContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </StrictMode>
 )

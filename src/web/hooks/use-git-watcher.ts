afba3fbebf66592d5a3ac7218b656ad8b42853c6
José Henrique Leão <77419998+jhleao@users.noreply.github.com>
2025-12-10 08:23:33 -0300

    chore: temporarily disable git watcher (#91)

--- src/web/hooks/use-git-watcher.ts
+++ src/web/hooks/use-git-watcher.ts
@@ -1,36 +1,27 @@
-import { useEffect } from 'react'
-
-export function useGitWatcher({
-  repoPath,
-  onRepoChange,
-  onRepoError
-}: {
+export function useGitWatcher(_: {
   repoPath: string | null
   onRepoChange: () => void
   onRepoError?: (error: string) => void
 }): void {
-  useEffect(() => {
-    if (!repoPath) return
-
-    void window.api.watchRepo({ repoPath })
-
-    const cleanupListener = window.api.onRepoChange(() => {
-      onRepoChange()
-    })
-
-    let cleanupErrorListener: (() => void) | undefined
-    if (onRepoError) {
-      cleanupErrorListener = window.api.onRepoError((error) => {
-        onRepoError(error)
-      })
-    }
-
-    return () => {
-      cleanupListener()
-      if (cleanupErrorListener) {
-        cleanupErrorListener()
-      }
-      void window.api.unwatchRepo({ repoPath })
-    }
-  }, [repoPath, onRepoChange, onRepoError])
+  // Disabling this for now until we resolve all the UI blinking during complex operations
+  // useEffect(() => {
+  //   if (!repoPath) return
+  //   void window.api.watchRepo({ repoPath })
+  //   const cleanupListener = window.api.onRepoChange(() => {
+  //     onRepoChange()
+  //   })
+  //   let cleanupErrorListener: (() => void) | undefined
+  //   if (onRepoError) {
+  //     cleanupErrorListener = window.api.onRepoError((error) => {
+  //       onRepoError(error)
+  //     })
+  //   }
+  //   return () => {
+  //     cleanupListener()
+  //     if (cleanupErrorListener) {
+  //       cleanupErrorListener()
+  //     }
+  //     void window.api.unwatchRepo({ repoPath })
+  //   }
+  // }, [repoPath, onRepoChange, onRepoError])
 }

1d3afaee2211abcd641f20dc8cd408eb6dbc0e79
jhleao <77419998+jhleao@users.noreply.github.com>
2025-12-23 09:59:41 +0100

    chore: modify update toasts as per lack of code signing

--- src/web/hooks/use-update-notifications.ts
+++ src/web/hooks/use-update-notifications.ts
@@ -1,23 +1,31 @@
 import { useEffect } from 'react'
 import { toast } from 'sonner'
 
+const RELEASES_URL = 'https://github.com/jhleao/teapot/releases'
+
 export function useUpdateNotifications(): void {
   useEffect(() => {
     const cleanupDownloading = window.api.onUpdateDownloading((version) => {
-      toast.info('Update available', {
-        description: `Downloading version ${version}...`
+      // Since we don't have Apple code signing yet, the auto updater fails to auto install.
+      // So we just prompt the user to uninstall+reinstall cleanly. Instead.
+      // Once code signing is added, this can just be uncommented as the flow will work again.
+      // toast.info('Update available', {
+      //   description: `Downloading version ${version}...`
+      // })
+      toast.info('New update available', {
+        description: `Head to ${RELEASES_URL} to download the latest version (${version}).`
       })
     })
 
-    const cleanupDownloaded = window.api.onUpdateDownloaded((version) => {
-      toast.success('Update ready', {
-        description: `Version ${version} will be installed when you restart the app`
-      })
-    })
+    // const cleanupDownloaded = window.api.onUpdateDownloaded((version) => {
+    //   toast.success('Update ready', {
+    //     description: `Version ${version} will be installed when you restart the app`
+    //   })
+    // })
 
     return () => {
       cleanupDownloading()
-      cleanupDownloaded()
+      // cleanupDownloaded()
     }
   }, [])
 }

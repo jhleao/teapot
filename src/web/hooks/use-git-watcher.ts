27dfd8f3a8ce2bb14eb43701148ba77d9424df45
jhleao <77419998+jhleao@users.noreply.github.com>
2025-12-23 16:51:30 +0100

    feat: restore git watcher functionality with rebase intent lock

--- src/web/hooks/use-git-watcher.ts
+++ src/web/hooks/use-git-watcher.ts
@@ -1,27 +1,32 @@
-export function useGitWatcher(_: {
+import { useEffect } from 'react'
+
+export function useGitWatcher({
+  onRepoChange,
+  repoPath,
+  onRepoError
+}: {
   repoPath: string | null
   onRepoChange: () => void
   onRepoError?: (error: string) => void
 }): void {
-  // Disabling this for now until we resolve all the UI blinking during complex operations
-  // useEffect(() => {
-  //   if (!repoPath) return
-  //   void window.api.watchRepo({ repoPath })
-  //   const cleanupListener = window.api.onRepoChange(() => {
-  //     onRepoChange()
-  //   })
-  //   let cleanupErrorListener: (() => void) | undefined
-  //   if (onRepoError) {
-  //     cleanupErrorListener = window.api.onRepoError((error) => {
-  //       onRepoError(error)
-  //     })
-  //   }
-  //   return () => {
-  //     cleanupListener()
-  //     if (cleanupErrorListener) {
-  //       cleanupErrorListener()
-  //     }
-  //     void window.api.unwatchRepo({ repoPath })
-  //   }
-  // }, [repoPath, onRepoChange, onRepoError])
+  useEffect(() => {
+    if (!repoPath) return
+    void window.api.watchRepo({ repoPath })
+    const cleanupListener = window.api.onRepoChange(() => {
+      onRepoChange()
+    })
+    let cleanupErrorListener: (() => void) | undefined
+    if (onRepoError) {
+      cleanupErrorListener = window.api.onRepoError((error) => {
+        onRepoError(error)
+      })
+    }
+    return () => {
+      cleanupListener()
+      if (cleanupErrorListener) {
+        cleanupErrorListener()
+      }
+      void window.api.unwatchRepo({ repoPath })
+    }
+  }, [repoPath, onRepoChange, onRepoError])
 }
